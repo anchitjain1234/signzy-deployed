bcdbda262e7fe101593cb3cd97b39de373d1b1b6
Anchit Jain <anchitjain1234@gmail.com>
2015-06-13 12:05:04 +0530

    Added MVC files

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,4 +31,20 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+
+    public $pageTitle;
+
+
+
+    public $components = array(
+        'Session',
+        'Auth' => array(
+            'authenticate' => array(
+                'Form' => array(
+                    'passwordHasher' => 'Blowfish'
+                    )
+                )
+            )
+        );
+
 }
