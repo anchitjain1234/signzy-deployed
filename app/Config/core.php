e58cf94597994f12e505ad34a6c74f2c9c03a1b9
Anchit Jain <anchitjain1234@gmail.com>
2015-07-22 23:10:48 +0530

    CHanged debug mode to 0

--- app/Config/core.php
+++ app/Config/core.php
@@ -31,7 +31,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
+	Configure::write('debug', 0);
 date_default_timezone_set('Asia/Calcutta');
 /**
  * Configure the Error handler used to handle errors for your application. By default
