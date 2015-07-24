13d04ed2f0e345365273e9319aec6c3de322aa24
Anchit Jain <anchitjain1234@gmail.com>
2015-07-24 09:42:43 +0000

    Unknown changes

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -408,7 +408,7 @@ class UsersController extends AppController {
 
         $email_queue_localhost = $sqs_client->createQueue(array('QueueName' => 'localhost_emails'));
         $email_queue_localhost_url = $email_queue_localhost->get('QueueUrl');
-//        $this->log("send email running");
+        $this->log("send email running");
         $receive_email = $sqs_client->receiveMessage(array(
             'QueueUrl' => $email_queue_localhost_url,
             'MaxNumberOfMessages' => 5,
@@ -421,8 +421,8 @@ class UsersController extends AppController {
                 $message_receipt_handle = $message['ReceiptHandle'];
                 $userdata = array();
 //                CakeLog::write('emails', $body);
-//                $this->log("email body");
-//                $this->log($body);
+                $this->log("email body");
+                $this->log($body);
                 $userdata['User']['name'] = $body->user_name;
                 $userdata['User']['username'] = $body->user_username;
                 if ($this->send_general_email($userdata, $body->link, $body->title, $body->content, $body->subject, $body->button_text)) {
